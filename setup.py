628488ae6caa769515d86dcfba6ce00afac281a8
Balasubramaniant <balasubramaniant.9507@gmail.com>
2019-11-23 23:00:37 +0530

    adding setup.py for future use

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='yts-api',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Balasubramanian',
+    author_email='balasubramaniant.9507@gmail.com',
+    description=''
+)
