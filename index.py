4ff35b25e3b5ce94e903ee7c00d8b8cf61b10dc5
Balasubramaniant <balasubramaniant.9507@gmail.com>
2019-11-23 18:08:03 +0530

    initial framework. working most downloaded movies version

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import json
+import sys
 from urllib import request
+import argparse
 
 url = "https://yts.tl/api/"
 headers = {
@@ -7,19 +9,71 @@ headers = {
 }
 
 
-def list_movies():
+def call_api(uri):
     global headers
     try:
-        uri = url + "v2/list_movies.json"
         req = request.Request(method="GET", url=uri, headers=headers)
         req = request.urlopen(req)
-        print(json.loads(req.read()))
+        return json.loads(req.read())
     except Exception as e:
+        print("YTS.LT api call Failed with exception- ", end="")
         print(str(e))
+        sys.exit(1)
+
+
+def get_movies(req_uri, limit=50):
+    # setting limit to 50 which is yts.lt api's limit on list movies api call.
+    page_limit_counter_list = [50] * divmod(limit, 50)[0]
+    if divmod(limit, 50)[1] != 0:
+        page_limit_counter_list.append(divmod(limit, 50)[1])
+
+    page_counter = 1
+
+    return_data = []
+    for result_count in page_limit_counter_list:
+        try:
+            uri = url + "{}&page={}&limit={}".format(req_uri, page_counter, result_count)
+            req_resp = call_api(uri=uri)
+
+            if req_resp["status"] != 'ok':
+                print("YTS.LT Api Failure at page {}".format(page_counter))
+                print("Returned Status = {}".format(req_resp["status"]))
+                print("Returned Status Message = {}".format(req_resp["status_message"]))
+                page_counter += 1
+                continue
+
+            if req_resp["data"]["movies"]:
+                return_data.extend(req_resp["data"]["movies"])
+            page_counter += 1
+
+        except Exception as e:
+            page_counter += 1
+            print("get_movies() failed at page {} with exception- ".format(page_counter - 1), end="")
+            print(str(e))
+            continue
+
+    return return_data
+
+
+def pretty_print_movies(data):
+    print("{:<10} - {:<50} - {:<10} - {:<15} - {:<15} - {:<100}".format("index_id", "title", "year", "imdb_rating", "mpa_rating", "download options"))
+    for entry in data:
+        index_id = entry["id"]
+        title = entry["title"]
+        year = entry["year"]
+        imdb_rating = entry["rating"]
+        mpa_rating = entry["mpa_rating"]
+        available_quality = []
+        for option in entry["torrents"]:
+            quality = option["quality"] + '|' + option["type"] + ' - ' + option["size"]
+            available_quality.append(quality)
+
+        print("{:<10} - {:<50} - {:<10} - {:<15} - {:<15} - {}".format(index_id, title, year, imdb_rating, mpa_rating, available_quality))
 
 
 def main():
-    list_movies()
+    data = get_movies(req_uri="v2/list_movies.json?sort_by=download_count", limit=10)
+    pretty_print_movies(data=data)
 
 
 if __name__ == '__main__':
